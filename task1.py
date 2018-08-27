9647e4d93d984a27370c49887b768cabfff4332f
Bozhao Liu <liubozhao@gmail.com>
2018-08-27 21:47:59 +0200

    coding enhancement with formating

--- task1.py
+++ task1.py
@@ -4,18 +4,29 @@ Created on Mon Aug 27 16:34:28 2018
 
 @author: Tommy
 """
+import math
+import numpy
+import matplotlib
 
-from math import cos
-from math import sin
+x = float(input('Please input the width of the gap: '))
+y = float(input('please input the distance you wanna go: '))
 
-for i in range(1,90): #for hver vinkel
+Min_angle = math.atan(x/(y/2))
+Max_angle = math.pi/2
 
-    c = 15/cos(i) #regner ut hypotenus
-    b = sin(i)*c #regner ut motstående katet til vinkel
-    s_p = 200 - b #regner ut lengde promerade når man kommer opp til den
-    t_1 = (c + 15)/0.8 #regner tid for å gå i sand, hypotenus og 15 meter
-    t_2 = s_p / 3 #regner ut tid brukt på å gå i promerade
-    t = t_1 + t_2 #tid totalt
-    print(min(t))
-    
-# må ta vinkler til radianer
+alpha = numpy.linspace(Min_angle, Max_angle, 1000)
+Tid = y/0.8 #initial time set as go directly to the end
+ang = 0
+
+for I in alpha:
+    if Tid > ((y - 2*x/(numpy.tan(I)))/3.0 + (2*x/numpy.sin(I))/0.8):
+        Tid = ((y - 2*x/(numpy.tan(I)))/3.0 + (2*x/numpy.sin(I))/0.8)
+        ang = I
+        
+print('the minimum time is %.1f' % Tid) 
+print('the angle should be %.1f' % math.degrees(ang), ' degrees')
+print('which is %.1f' % ang, ' radians')
+
+Tid = (y - 2*x/(numpy.tan(alpha)))/3.0 + (2*x/numpy.sin(alpha))/0.8
+
+matplotlib.pyplot.plot(alpha, Tid)
